1cfeb9eb3aecb3f9898825cd4154d1fc48397a6e
Alexander Heyes <32589919+heyesalex@users.noreply.github.com>
2017-11-01 14:51:20 +1300

    Make features toggleable, based on settings doc
    Can set width, height, initial number of counters, radius, and toggle equation display and adding counters on mouseclick using Interactive Settings.js file defining those variables.

--- Integer Counters.js
+++ Integer Counters.js
@@ -1,25 +1,51 @@
 let counters=[];
-let r=50;
+//let r=40;
 let dx;
 let dy;
 let l = counters.length;
 let toHighlight;
 let toFade;
+let oneZero;
+let dispA = initRed;
+let dispB = initBlue;
+let dispTot = dispA-dispB;
+var h1;
+var addRed = true;
+
 
 function setup() {
-	createCanvas(640, 480);
+	createCanvas(setWidth, setHeight);
     
-    for(let i=0 ; i<3 ; i++){
-        let c = new Counter(100, i*150+100, r, 0, 0, 255);
+//Define initial array of counters    
+    for(let i=0 ; i< initRed+initBlue ; i++){
+        let col;
+        if(i<initRed){
+            col = 0;
+        } else {
+            col = 1;
+        }
+        let c = new Counter(i*100+100, random(100, height-100), r, (1-col)*255, 0, col*255);
         l = counters.push(c);
+    }
+    
+//Define Equation Display as html element
+    if(displayEquation){
+        h1 = createElement('h1' , dispA+'+(-'+dispB + ')=' + dispTot);    
     }
 }
 
+
+
+
+
 function draw(){
+    
     background(255);    
     
+//Looking at each counter in the array in turn, from bottom to top.
     for(let i=0 ; i<l ; i++){
             
+//Check if counters should be highlighted.
         push();
         if(l>0){
             if(counters[l-1].highlighted){
@@ -29,9 +55,12 @@ function draw(){
             }
         }
         
+//Check if counters should be fading out. 
         if(counters[i].fading){
             noStroke();
             counters[i].fade();
+            
+//If fully faded, remove counter from array and update array length.
             if(counters[i].alpha <=0){
                 counters.splice(i, 1);
                 i--;
@@ -40,15 +69,54 @@ function draw(){
                 continue;
             }
         }
+            
+//Draw counters.
         counters[i].show();
         pop();
+        
+//Update red and blue count for display.
+        if(displayEquation){
+            if(counters[i].R==255){
+                dispA++;
+            }
+            if(counters[i].B==255){
+                dispB++;
+            }
+        }
     }
     
-//  Move counter
-    if(l>0){
-        if(counters[l-1].moving){
-        counters[l-1].drag(mouseX+dx, mouseY+dy);
+//Update displayed equation
+    if(displayEquation){
+        dispTot = dispA - dispB;
+        if(dispB==0){
+            h1.html(dispA+'+'+dispB + '=' + dispTot)
+        } else {
+            h1.html(dispA+'+(-'+dispB + ')=' + dispTot)
         }
+    //Reset displayed equation
+        dispA = 0;
+        dispB = 0;
+    }
+    
+    
+//  Move counter within boundaries.
+    if(l>0){
+        if(counters[l-1].moving){             
+            counters[l-1].drag(mouseX+dx, mouseY+dy);
+        }
+        
+        if(counters[l-1].x<=0){
+            counters[l-1].x = 0;
+        }
+        if(counters[l-1].x>=width){
+            counters[l-1].x = width;
+        }
+        if(counters[l-1].y<=0){
+            counters[l-1].y = 0;
+        }
+        if(counters[l-1].y>=height){
+            counters[l-1].y = height;
+        }      
     }
     
  //Check if overlapping. Set highlighting as true, set toHighlight as i, and break.
@@ -56,7 +124,7 @@ function draw(){
         if(counters[l-1].moving){
             for(let i=l-2 ; i>=0 ; i--){
                 counters[l-1].highlighted = false;            
-                if(counters[l-1].overlap(counters[i].x, counters[i].y) && !counters[i].fading && (counters[l-1].R!==counters[i].R)){
+                if(counters[l-1].overlap(counters[i].x, counters[i].y) && !counters[i].fading && (abs(counters[l-1].B - counters[i].B)==255)){
                     counters[l-1].highlighted = true;
                     toHighlight = i;
                     break;
@@ -67,27 +135,42 @@ function draw(){
 }
 
 
+
+
+
+
 function mousePressed(){
+    if(mouseButton == LEFT){
     
 //If click inside, define dx, dy as position of counter relative to mouse. Move to end of array. 
-    for(i = l-1 ; i>=0 ; i--){
-        if((counters[i].touching(mouseX, mouseY)) && (!counters[i].fading)){
-            if(top!==l-1){
-                counters.push(counters.splice(i, 1)[0]);
+        for(i = l-1 ; i>=0 ; i--){
+            if((counters[i].touching(mouseX, mouseY)) && (!counters[i].fading)){
+                if(top!==l-1){
+                    counters.push(counters.splice(i, 1)[0]);
+                }
+                counters[l-1].moving = true;
+                dx = counters[l-1].x - mouseX;
+                dy = counters[l-1].y - mouseY;
+                break;
             }
-            counters[l-1].moving = true;
-            dx = counters[l-1].x - mouseX;
-            dy = counters[l-1].y - mouseY;
-            break;
         }
-    }
-    
-    if(l==0 || !counters[l-1].moving){
-            let c = new Counter(mouseX, mouseY, r, 255, 0, 0)
-            l = counters.push(c);
-            counters[l-1].moving = true;
-            dx=0;
-            dy=0;
+        if(addCounters){
+            if(l==0 || !counters[l-1].moving){
+                if(addRed){
+                    let c = new Counter(mouseX, mouseY, r, 255, 0, 0);
+                    l = counters.push(c);
+                    counters[l-1].moving = true;
+                    dx=0;
+                    dy=0;
+                } else {
+                    let c = new Counter(mouseX, mouseY, r, 0, 0, 255);
+                    l = counters.push(c);
+                    counters[l-1].moving = true;
+                    dx=0;
+                    dy=0;                
+                }
+            }
+        }
     }
 }
 
