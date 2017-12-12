a91add3533a124d426b56943572dc4835637aae7
Alexander Heyes <32589919+heyesalex@users.noreply.github.com>
2017-12-12 16:18:07 +1300

    Improved optional settings, retrieve from hash
    Optional settings can be set by url hash. These get passed into an array with four settings dictating number of red and blue counters, option to add more counters, and option to display a running total of the counters as integers.

--- Interactive Settings.js
+++ Interactive Settings.js
@@ -1,8 +1,36 @@
-var initRed = 3;
-var initBlue = 3;
-var setWidth = 640;
+//Variables = [initRed, initBlue, addCounters?0:1, displayEquation?0:1]
+
+var variables = window.location.hash.substr(1);
+variables = variables.split('_')
+var settings = {
+    initRed: +variables[0],
+    initBlue: +variables[1]
+}
+
+//Counter settings
+
+var setWidth = 800;
 var setHeight = 300;
-var r = 40;
-var addCounters = true;
-var displayEquation = true;
-var toggleColour = true;
+var r = 35;
+
+var addCounters, displayEquation;
+var rbStack = true;//This should ultimately be subsumed under addCounters
+
+//Check if adding counters and displaying equations has been specified. If not, default to having these ON
+if(!variables[2]){
+    settings.addCounters = true;
+    settings.displayEquation = true;
+} else{
+    
+    if(variables[2] == 0){
+        settings.addCounters = false;
+    } else {
+        settings.addCounters = true;
+    }
+    
+    if(variables[3] == 0){
+        settings.displayEquation = false;
+    } else {
+        settings.displayEquation = true;
+    }
+}
