33e50e50c6423a8e0b07ce12943f2ab314962bb6
Alexander Heyes <32589919+heyesalex@users.noreply.github.com>
2017-12-14 15:46:04 +1300

    Updated alpha, removed highlight function
    Highlighting now happens in show, and alpha can be specified explicitly.

--- Counters.js
+++ Counters.js
@@ -1,33 +1,46 @@
 class Counter{
 
 //Each counter is an ellipse with centre (x, y), radius r, colour RGB, and it knows if it should be moving. 
-    constructor(x, y, r, R, G, B){
+    constructor(x, y, r, col, alpha){
         this.x=x;
         this.y=y;
         this.r=r;
-        this.R=R;
-        this.G=G;
-        this.B=B;
+        this.col = col;
         this.moving = false;
         this.highlighted = false;
         this.fading = false;
-        this.alpha = 255;
+        
+        if(!alpha){
+            this.alpha = 255;
+        } else {
+            this.alpha = alpha;
+        }
     }
     
 //Each ellipse gets drawn with its specified colour.    
     show(){
-        fill(this.R, this.G, this.B, this.alpha);
+        
+        if(this.fading){
+            noStroke();
+        } else if(this.highlighted){
+            strokeWeight(6);
+            stroke(251, 102, 17, this.alpha);
+        }
+        
+        fill(this.col.R, this.col.G, this.col.B, this.alpha);
         ellipse(this.x, this.y, 2*this.r);
     }
 
 //This moves the counter by redefining its position.   
     drag(newx, newy){
+            if(this.moving){
                 this.x=newx;
                 this.y=newy;
+            }
     }
     
-    overlap(x, y){
-        let d=dist(this.x, this.y, x, y);
+    overlap(counter){
+        let d=dist(this.x, this.y, counter.x, counter.y);
         return Boolean(d <= 2*this.r);
     }
     
@@ -38,10 +51,5 @@ class Counter{
     
     fade(){
             this.alpha-=10;
-    }
-    
-    highlight(){
-            strokeWeight(6);
-            stroke(251, 102, 17, this.alpha);
     }
 }
