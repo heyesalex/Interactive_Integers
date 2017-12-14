011a33a8e3792500b96cfa3d526bca98c60e1927
Alexander Heyes <32589919+heyesalex@users.noreply.github.com>
2017-12-14 15:43:34 +1300

    Cleaner and clearer settings
    Makes hash settings optional, with default to 4R, 2B, adding = true, and display = true.

--- Interactive Settings.js
+++ Interactive Settings.js
@@ -1,36 +1,30 @@
-//Variables = [initRed, initBlue, addCounters?0:1, displayEquation?0:1]
-
 var variables = window.location.hash.substr(1);
 variables = variables.split('_')
-var settings = {
-    initRed: +variables[0],
-    initBlue: +variables[1]
+var settings = {};
+
+
+//Check url hash settings. If not available, default to 4R, 2B, add=true and display = true
+if(variables[0] && variables [1]){
+    settings.initRed = variables[0];  
+    settings.initBlue = variables[1];
+
+    if(variables[2] && variables[3]){
+        settings.addCounters=Boolean(variables[2]);
+        settings.displayEquation = Boolean(variables[3]);
+    } else {
+        settings.addCounters = true;
+        settings.displayEquation = true;
+    }
+    
+} else {
+    settings.initRed = 4;
+    settings.initBlue = 2;
+    settings.addCounters = true;
+    settings.displayEquation = true;
 }
 
-//Counter settings
+var rbStack = true;//To be subsumed under addCounters
 
 var setWidth = 800;
 var setHeight = 300;
 var r = 35;
-
-var addCounters, displayEquation;
-var rbStack = true;//This should ultimately be subsumed under addCounters
-
-//Check if adding counters and displaying equations has been specified. If not, default to having these ON
-if(!variables[2]){
-    settings.addCounters = true;
-    settings.displayEquation = true;
-} else{
-    
-    if(variables[2] == 0){
-        settings.addCounters = false;
-    } else {
-        settings.addCounters = true;
-    }
-    
-    if(variables[3] == 0){
-        settings.displayEquation = false;
-    } else {
-        settings.displayEquation = true;
-    }
-}
