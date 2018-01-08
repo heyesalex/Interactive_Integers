6f0a297ff707370e67b0227dd5f11ca45ed5cc0d
Alexander Heyes <32589919+heyesalex@users.noreply.github.com>
2018-01-08 13:43:53 +1300

    Make settings more intuitive, include subtraction

--- Interactive Settings.js
+++ Interactive Settings.js
@@ -1,29 +1,40 @@
+//The variables hash is in the format #n1_n2_B1_B2_B3 where the first two give the initial number of counters, and the remaining
+
 var variables = window.location.hash.substr(1);
-variables = variables.split('_')
+variables = variables.split('_');
 var settings = {};
 
 
 //Check url hash settings. If not available, default to 4R, 2B, add=true and display = true
-if(variables[0] && variables [1]){
-    settings.initRed = variables[0];  
-    settings.initBlue = variables[1];
-
-    if(variables[2] && variables[3]){
-        settings.addCounters=Boolean(variables[2]);
-        settings.displayEquation = Boolean(variables[3]);
-    } else {
-        settings.addCounters = true;
-        settings.displayEquation = true;
-    }
-    
+if(variables[0] && Number.isInteger(+variables[0])){
+    settings.initRed = +variables[0];
 } else {
-    settings.initRed = 4;
-    settings.initBlue = 2;
-    settings.addCounters = true;
-    settings.displayEquation = true;
+    settings.initRed = Math.floor(Math.random()*5+1);
 }
 
-var rbStack = true;//To be subsumed under addCounters
+if(variables[1] && Number.isInteger(+variables[1])){
+    settings.initBlue = +variables[1];
+} else {
+    settings.initBlue = Math.floor(Math.random()*5+1);
+}
+
+if(variables[2] == '0' || variables[2] == '1'){
+    settings.addCounters=Boolean(+variables[2]);
+} else {
+    settings.addCounters = true;
+}
+
+if(variables[3] == '0' || variables[3] == '1'){
+    settings.subtractCounters = Boolean(+variables[3]);
+} else {
+    settings.subtractCounters = true;
+}
+
+if(variables[4] == '0' || variables[4] == '1'){
+    settings.displayEquation = Boolean(+variables[4]);        
+} else {
+    settings.displayEquation = true;
+}
 
 var setWidth = 800;
 var setHeight = 300;
